262969c551f2c0e16629a1eefea166dd3945766d
Ozan SAYILIR <ozanysayilir@gmail.com>
2020-02-08 16:27:44 +0300

    delegate

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,34 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        //Delegeta ile alınan veriler aynı olmak zorunda
+
+        // action ve Func ile kullanılır action daha hızlı çalışır çünkü return fonksiyonu yok.
+
+        public static string ConcattedString = string.Empty;
+       
+    public static void Concat(string ilk, string son)
+        {
+            ConcattedString = ilk + son;
+            Console.WriteLine(ilk + son);
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Action<string, string> testDelegate=Concat;
+            testDelegate("x", "y");
+
+             
+            Console.WriteLine("Singleton'dan gelen nesne: " +ConcattedString);
+
+           
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
